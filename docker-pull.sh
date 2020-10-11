e374d9c6e64c11ae17a6593daf1e91a93513f4e8
Z1P TI3 <z1pti3@outlook.com>
2020-10-11 14:33:00 +0100

    Fixed debploy bugs

--- docker-pull.sh
+++ docker-pull.sh
@@ -3,3 +3,10 @@
 docker pull mongo:latest
 docker pull z1pti3/jimi_core:amd64
 docker pull z1pti3/jimi_web:amd64
+
+docker run -d -v /home/ubuntu/jimi/db:/jimi/db --name jimi_db mongo:latest
+docker run -d -v /home/ubuntu/jimi/data:/jimi/data --name jimi_core z1pti3/jimi_core:amd64
+docker run -d -p 5002:5002 -v /home/ubuntu/jimi/data:/jimi/data --name jimi_web z1pti3/jimi_web:amd64
+
+
+docker logs jimi_core
