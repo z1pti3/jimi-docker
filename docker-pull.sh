8dd2c00286b34cd7ce1dac13034e8ab62cba677a
Z1P TI3 <z1pti3@outlook.com>
2020-10-11 14:47:41 +0100

    fixes

--- docker-pull.sh
+++ docker-pull.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
+docker stop jimi_core
+docker stop jimi_web
+docker stop jimi_db
+
+docker rm jimi_core
+docker rm jimi_web
+docker rm jimi_db
+
 docker pull mongo:latest
 docker pull z1pti3/jimi_core:amd64
 docker pull z1pti3/jimi_web:amd64
 
 docker run -d -v /home/ubuntu/jimi/db:/data/db --name jimi_db mongo:latest
-docker run -d -v /home/ubuntu/jimi/data:/jimi/data --name jimi_core z1pti3/jimi_core:amd64
-docker run -d -p 5002:5002 -v /home/ubuntu/jimi/data:/jimi/data --name jimi_web z1pti3/jimi_web:amd64
-
+docker run -it -d -v /home/ubuntu/jimi/data:/jimi/data --name jimi_core z1pti3/jimi_core:amd64
+docker run -it -d -p 5002:5002 -v /home/ubuntu/jimi/data:/jimi/data --name jimi_web z1pti3/jimi_web:amd64
 
 docker logs jimi_core
