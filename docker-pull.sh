e454e546d680966059d893803bc469d2403b3c69
Z1P TI3 <z1pti3@outlook.com>
2020-10-11 18:27:27 +0100

    Added missing network

--- docker-pull.sh
+++ docker-pull.sh
@@ -16,8 +16,9 @@ docker pull mongo:latest
 docker pull z1pti3/jimi_core:amd64
 docker pull z1pti3/jimi_web:amd64
 
-docker run -d -v /home/ubuntu/jimi/db:/data/db --name jimi_db mongo:latest
-docker run -it -d -v /home/ubuntu/jimi/data:/jimi/data --name jimi_core z1pti3/jimi_core:amd64
-docker run -it -d -p 5002:5002 -v /home/ubuntu/jimi/data:/jimi/data --name jimi_web z1pti3/jimi_web:amd64
+docker network create jimi_network
+docker run -d -v /home/ubuntu/jimi/db:/data/db --net jimi_network -p --name jimi_db mongo:latest
+docker run -it -d -v /home/ubuntu/jimi/data:/jimi/data -v /home/ubuntu/jimi/plugins:/jimi/plugins --net jimi_network --name jimi_core z1pti3/jimi_core:amd64
+docker run -it -d -p 5002:5002 -v /home/ubuntu/jimi/data:/jimi/data -v /home/ubuntu/jimi/plugins:/jimi/plugins --net jimi_network --name jimi_web z1pti3/jimi_web:amd64
 
 docker logs jimi_core
