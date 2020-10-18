6cb6b8a55cb64ac586f642822d87072f3c0f714a
Z1P TI3 <z1pti3@outlook.com>
2020-10-18 13:19:50 +0100

    Fixed mount location

--- docker-pull.sh
+++ docker-pull.sh
@@ -17,8 +17,8 @@ docker pull z1pti3/jimi_core:amd64
 docker pull z1pti3/jimi_web:amd64
 
 docker network create jimi_network
-docker run -d -v /home/ubuntu/jimi/db:/data/db --net jimi_network -p --name jimi_db mongo:latest
-docker run -it -d -v /home/ubuntu/jimi/data:/jimi/data -v /home/ubuntu/jimi/plugins:/jimi/plugins --net jimi_network --name jimi_core z1pti3/jimi_core:amd64
-docker run -it -d -p 5002:5002 -v /home/ubuntu/jimi/data:/jimi/data -v /home/ubuntu/jimi/plugins:/jimi/plugins --net jimi_network --name jimi_web z1pti3/jimi_web:amd64
+docker run -d -v /home/ubuntu/jimi/db:/data/db --net jimi_network --name jimi_db mongo:latest
+docker run -it -d -v /home/ubuntu/jimi/data:/home/jimi/jimi/data -v /home/ubuntu/jimi/plugins:/home/jimi/jimi/plugins --net jimi_network --name jimi_core z1pti3/jimi_core:amd64
+docker run -it -d -p 5002:5002 -v /home/ubuntu/jimi/data:/home/jimi/jimi/data -v /home/ubuntu/jimi/plugins:/home/jimi/jimi/plugins --net jimi_network --name jimi_web z1pti3/jimi_web:amd64
 
 docker logs jimi_core
