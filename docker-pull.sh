eae71fddf4c9a11316c786c25747ba041dead00c
z1pti3 <z1pti3@outlook.com>
2020-12-27 16:45:42 +0000

    Updated build

--- docker-pull.sh
+++ docker-pull.sh
@@ -19,6 +19,6 @@ docker pull z1pti3/jimi_web:amd64
 docker network create jimi_network
 docker run -d -v /home/ubuntu/jimi/db:/data/db --net jimi_network --name jimi_db mongo:latest
 docker run -it -d -v /home/ubuntu/jimi/data:/home/jimi/jimi/data -v /home/ubuntu/jimi/plugins:/home/jimi/jimi/plugins --net jimi_network --name jimi_core z1pti3/jimi_core:amd64
-docker run -it -d -p 5002:5002 -v /home/ubuntu/jimi/data:/home/jimi/jimi/data -v /home/ubuntu/jimi/plugins:/home/jimi/jimi/plugins --net jimi_network --name jimi_web z1pti3/jimi_web:amd64
+docker run -it -d -p 443:443 -v /home/ubuntu/jimi/data:/home/jimi/jimi/data -v /home/ubuntu/jimi/plugins:/home/jimi/jimi/plugins --net jimi_network --name jimi_web z1pti3/jimi_web:amd64
 
 docker logs jimi_core
